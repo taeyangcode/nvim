8e55cdd98ebd474eb54247adf99e1495984a6d42
taeyangcode <coreymostero@gmail.com>
2023-01-25 15:27:52 -0800

    osx clipboard yank

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -25,3 +25,13 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- clipboard yank osx
+vim.cmd[[
+    vnoremap \y y:call system("pbcopy", getreg("\""))<CR>
+    nnoremap \p :call setreg("\"", system("pbpaste"))<CR>p
+    
+    noremap YY "+y<CR>
+    noremap P "+gP<CR>
+    noremap XX "+x<CR>
+]]

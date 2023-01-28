473e4bf1aefa71b61712f3ba23ca8a55ae96a96b
taeyangcode <coreymostero@gmail.com>
2023-01-27 17:38:17 -0800

    updates

--- lua/core/config/mason.lua
+++ lua/core/config/mason.lua
@@ -9,28 +9,29 @@ require'lspconfig'.html.setup{} -- npm i -g vscode-langservers-extracted
 require'lspconfig'.jsonls.setup{}
 
 -- rust
-require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.rust_analyzer.setup {
+    -- cmd = {
+    --     "rustup", "run", "stable", "rust-analyzer"
+    -- }
+}
 
 -- typescript
 require'lspconfig'.tsserver.setup{} -- npm i -g typescript typescript-language-server
 
 -- lua (for nvim)
-require'lspconfig'.sumneko_lua.setup {
+require('lspconfig').sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
       },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
--- lua/core/config/nvim-tree.lua
+++ lua/core/config/nvim-tree.lua
@@ -1,3 +1,3 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup{}
 
 vim.keymap.set('n', '<c-f>', ':NvimTreeFindFileToggle<CR>')

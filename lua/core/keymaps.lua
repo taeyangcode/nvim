4bda5be853049b1427ef369140c7745dd407e942
taeyangcode <coreymostero@gmail.com>
2023-01-30 14:59:49 -0800

    osx updates

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,19 +19,5 @@ vim.cmd[[colorscheme iceberg]]
 -- encoding
 vim.cmd('filetype plugin indent on')
 
--- telescope keymaps
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- clipboard yank osx
-vim.cmd[[
-    vnoremap \y y:call system("pbcopy", getreg("\""))<CR>
-    nnoremap \p :call setreg("\"", system("pbpaste"))<CR>p
-    
-    noremap YY "+y<CR>
-    noremap P "+gP<CR>
-    noremap XX "+x<CR>
-]]
+-- clipboard
+vim.cmd('set clipboard+=unnamedplus')

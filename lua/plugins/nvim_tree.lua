11013743cd9a61b5c163bc01abb2395ee3084768
taeyangcode <coreymostero@gmail.com>
2023-02-14 18:54:46 -0800

    added config to nvimtree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -15,9 +15,23 @@ return {
         local nvim_tree = require "nvim-tree"
 
         nvim_tree.setup {
-          disable_netrw = true,
-          auto_reload_on_write = true,
-          hijack_cursor = true,
+            disable_netrw = true,
+            auto_reload_on_write = true,
+            hijack_cursor = true,
+            git = {
+                enable = true,
+            },
+            renderer = {
+                highlight_git = true,
+                icons = {
+                    show = {
+                        git = true,
+                    },
+                },
+            },
+            view = {
+                side = "left",
+            },
         }
     end,
 }

39cdd92f69545ada4f770a47036d2b1275ec4ef2
taeyangcode <coreymostero@gmail.com>
2023-02-16 16:42:48 -0800

    added vim-latex warnings to ignore

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -11,5 +11,18 @@ return {
         end
 
         vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+
+        vim.cmd([[
+            let g:Tex_IgnoredWarnings = 
+                \'Underfull'."\n".
+                \'Overfull'."\n".
+                \'specifier changed to'."\n".
+                \'You have requested'."\n".
+                \'Missing number, treated as zero.'."\n".
+                \'There were undefined references'."\n".
+                \'Citation %.%# undefined'."\n".
+                \'Double space found.'."\n"
+            let g:Tex_IgnoreLevel = 8
+        ]])
     end,
 }
